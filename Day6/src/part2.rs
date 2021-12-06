921d6c3a4f354446f246e6f7b7a1f4cea071276d
abhinavr93 <vanihba1993@protonmail.com>
2021-12-06 23:09:56 +0530

    Added Day 6 solutions

---
+++ Day6/src/part2.rs
@@ -0,0 +1,29 @@
+use std::fs::File;
+use std::io::Read;
+
+const N: usize = 256;
+
+pub fn solve(input: &mut File) {
+    let mut buf = String::new();
+    input.read_to_string(&mut buf);
+
+    let mut arr = vec![(0_u128, 0_u128); 7];
+
+    buf.split(',')
+        .filter_map(|c| c.parse::<usize>().ok())
+        .for_each(|t0| arr[t0].0 += 1);
+
+    println!("{:?}", arr);
+
+    for i in 1..=N {
+        let idx1 = (i + 1) % 7;
+        let idx2 = (i - 1) % 7;
+        arr[idx1].1 = arr[idx2].0;
+        arr[idx2].0 += arr[idx2].1;
+        arr[idx2].1 = 0;
+    }
+
+    let sum = arr.iter().fold(0, |sum, x| sum + x.0 + x.1);
+
+    println!("Final Number of Fishes: {}", sum);
+}
